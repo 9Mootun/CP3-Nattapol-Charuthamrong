e20653e91974ac3676ce084689c1ccd0c3677934
Nattapol <nattapolch.t@gmail.com.com>
2020-09-22 09:41:59 +0700

    Create Exercise4_Nattapol_C.py
    Exercise 4 Complete

---
+++ Exercise4_Nattapol_C.py
@@ -0,0 +1,9 @@
+ScoreFE = 80.5
+ScoreGB = 78.2
+ScoreICS = 88.5
+ScoreCP = 76.4
+print("---------- Your Score ----------")
+print(" Foundation English :",ScoreFE)
+print(" General Business   :",ScoreGB)
+print(" Introduction to Computer System :",ScoreICS)
+print(" Computer Programming :", ScoreCP)
