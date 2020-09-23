09ca8994cf810d73b03d92d1a7fde83243649799
Nattapol <nattapolch.t@gmail.com.com>
2020-09-23 15:21:31 +0700

    23/9/2020
    23/9/2020

---
+++ Lecture39.py
@@ -0,0 +1,17 @@
+Gradeinput = int(input("Enter your Score : "))
+if Gradeinput >= 80 :
+    print("Your Grade is A")
+elif Gradeinput >= 75 and Gradeinput < 80:
+    print("Your Grade is B+")
+elif Gradeinput >= 70 and Gradeinput < 75:
+    print("Your Grade is B")
+elif Gradeinput >= 65 and Gradeinput < 70:
+    print("Your Grade is C+")
+elif Gradeinput >= 60 and Gradeinput < 65:
+    print("Your Grade is C")
+elif Gradeinput >= 55 and Gradeinput < 60:
+    print("Your Grade is D+")
+elif Gradeinput >= 50 and Gradeinput < 55:
+    print("Your Grade is D")
+elif Gradeinput < 50:
+    print("Your Grade is F")
