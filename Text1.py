fd660c32a87a865943047575a65bb348f953ee43
Nattapol <nattapolch.t@gmail.com.com>
2020-09-30 21:45:37 +0700

    30/9/2020

--- Text1.py
+++ Text1.py
@@ -1,5 +1,13 @@
-list1 = [10,20,30,40,50]
-"""
+a = set(["a","b","c","d","e","f","g","a"])
+print(len(a))
+b = set("d",{"b","d"},"d","o",{"f","g"},"a")
+print(len(b))
+
+
+
+
+"""list1 = [10,20,30,40,50]
+
 print(type(list1[0]))
 for i in range(len(list1)):
     print(type(list1[i]))
@@ -7,7 +15,8 @@ for z in list1:
     print(type(z))
 for x in range(5):
     print(list1[x])
-    """
+
 numadd = int(input("Add num ="))
 list1.append(numadd)
 print(list1)
+"""
